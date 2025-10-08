01ad54146815082811f03d235b9090e7e0003f36
fleonce <mail@fleance.de>
2025-10-08 21:19:46 +0200

    Update activation checkpointing setup

--- ricode/ml/optimization/activation_checkpointing.py
+++ ricode/ml/optimization/activation_checkpointing.py
@@ -1,16 +1,12 @@
 from functools import partial
-
-import torch
+from typing import Any
 
 from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
     apply_activation_checkpointing,
     checkpoint_wrapper,
     CheckpointImpl,
 )
-from torch.utils.checkpoint import (
-    CheckpointPolicy,
-    create_selective_checkpoint_contexts,
-)
+from torch.utils.checkpoint import create_selective_checkpoint_contexts
 
 from ricode.ml.optimization.utils import guess_model_block_type
 from ricode.ml.training_types import ModelInitProtocol, TConfig, TModel
@@ -18,26 +14,14 @@ from ricode.ml.training_types import ModelInitProtocol, TConfig, TModel
 
 def setup_activation_checkpointing(
     model_init_fn: ModelInitProtocol[TConfig, TModel],
+    policy_fn: Any,
 ) -> ModelInitProtocol[TConfig, TModel]:
 
     def _model_init(config: TConfig) -> TModel:
-        aten = torch.ops.aten
-        compute_intensive_ops = [
-            aten.mm,
-            aten.bmm,
-            aten.addmm,
-        ]
-
-        def policy_fn(ctx, op, *args, **kwargs):
-            if op in compute_intensive_ops:
-                return CheckpointPolicy.MUST_SAVE
-            else:
-                return CheckpointPolicy.PREFER_RECOMPUTE
-
         non_reentrant_wrapper = partial(
             checkpoint_wrapper,
             checkpoint_impl=CheckpointImpl.NO_REENTRANT,
-            context_fn=create_selective_checkpoint_contexts(policy_fn),
+            context_fn=partial(create_selective_checkpoint_contexts, policy_fn),
         )
 
         module = model_init_fn(config)
