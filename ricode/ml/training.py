66579d72dab524be446e377288320c875cd41d84
fleonce <moritz.hennen@gmail.com>
2025-12-10 11:16:38 +0100

    Use the executable of the parent process to launch a pip freeze command

--- ricode/ml/training.py
+++ ricode/ml/training.py
@@ -388,7 +388,7 @@ def write_reproducibility_checkpoint(
     }
 
     with open(f"{model_path}/requirements.txt", "w") as f:
-        subprocess.run("python -m pip freeze".split(), stdout=f)
+        subprocess.run((sys.executable + " -m pip freeze").split(), stdout=f)
 
     torch.save(repro_dict, f"{model_path}/repro.pt")
     with open(f"{model_path}/hparams.json", "w") as f:
