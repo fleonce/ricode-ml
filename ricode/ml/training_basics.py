2216177a57219877a6eff24415435c074b6cbb53
fleonce <mail@fleance.de>
2026-02-03 16:08:47 +0100

    Avoid using repr if no custom function has been defined!

--- ricode/ml/training_basics.py
+++ ricode/ml/training_basics.py
@@ -31,7 +31,6 @@ import torch
 import typing_extensions
 from more_itertools.more import first
 from torch import Tensor
-from transformers import PreTrainedTokenizerBase
 from typing_extensions import Self
 
 try:
@@ -414,8 +413,8 @@ def conf_to_mapping(conf: Any):
     if attr.has(type(conf)):
         tgt = {}
         for attrib, attribute in attrs.fields_dict(type(conf)).items():
-            if attribute.repr:
-                tgt[attrib] = repr(getattr(conf, attrib))
+            if attribute.repr and not isinstance(attribute.repr, bool):
+                tgt[attrib] = attribute.repr(getattr(conf, attrib))
             else:
                 tgt[attrib] = conf_to_mapping(getattr(conf, attrib))
         return tgt
@@ -425,8 +424,6 @@ def conf_to_mapping(conf: Any):
         return {key: conf_to_mapping(value) for key, value in conf.items()}
     elif isinstance(conf, Sequence) and not isinstance(conf, str):
         return [conf_to_mapping(value) for value in conf]
-    elif isinstance(conf, PreTrainedTokenizerBase):
-        return conf.name_or_path
     else:
         return conf
 
