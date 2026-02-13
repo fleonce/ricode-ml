d52ffa84fb3e92bb9c52ecf7fd567de344973c37
fleonce <moritz.hennen@gmail.com>
2026-02-13 14:12:12 +0100

    Add some example functions for basic tokenization

---
+++ ricode/ml/_preprocessing/tokenization.py
@@ -0,0 +1,67 @@
+import functools
+import math
+from typing import Any, MutableMapping
+
+import numpy as np
+import torch
+from transformers import PreTrainedTokenizerBase
+
+__all__ = [
+    "dtype_for_tokenizer",
+    "reduce_to_num_tokens",
+    "tokenize_batch",
+]
+
+NUM_BYTES_TO_DTYPE = {
+    1: torch.uint8,
+    2: torch.uint16,
+    4: torch.uint32,
+    8: torch.uint64,
+}
+
+
+def dtype_for_tokenizer(tokenizer: PreTrainedTokenizerBase):
+    return _dtype_for_tokenizer(tokenizer.name_or_path, len(tokenizer))
+
+
+@functools.lru_cache
+def _dtype_for_tokenizer(tokenizer_name_or_path: str, size: int):
+    num_bytes = math.ceil(math.log2(size)) // 8
+
+    rounded_bytes = 2 ** math.ceil(math.log2(num_bytes))
+    if rounded_bytes < 1 or rounded_bytes > 8:
+        raise ValueError(
+            f"Tokenizer {tokenizer_name_or_path} requires more than {max(NUM_BYTES_TO_DTYPE.keys())} ({num_bytes}) to represent a single token (total size={size})"
+        )
+
+    return NUM_BYTES_TO_DTYPE[rounded_bytes]
+
+
+def _sample_to_tensor(sample: np.ndarray | list, dtype: torch.dtype):
+    if isinstance(sample, np.ndarray):
+        tensor = torch.from_numpy(sample).to(dtype)
+    else:
+        tensor = torch.tensor(sample, dtype=dtype)
+    return tensor
+
+
+def reduce_to_num_tokens(
+    batch: MutableMapping[str, list[Any]],
+) -> int:
+    return sum(map(len, batch["tokens"]))
+
+
+def tokenize_batch(
+    batch: MutableMapping[str, list[Any]],
+    /,
+    tokenizer: PreTrainedTokenizerBase,
+    min_length: int = 0,
+):
+    tokens_dtype = _dtype_for_tokenizer(tokenizer)
+    samples = tokenizer(batch["content"], truncation=True)["input_ids"]
+    tokens = [
+        _sample_to_tensor(sample, tokens_dtype)
+        for sample in samples
+        if min_length and len(sample) >= min_length
+    ]
+    return {"tokens": tokens}
