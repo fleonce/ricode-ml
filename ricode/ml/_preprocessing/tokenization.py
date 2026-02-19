6281db4f16653a67b3449feb4096aebf2a1eaae5
fleonce <moritz.hennen@gmail.com>
2026-02-19 15:30:48 +0100

    Add tokenization function for pre-tokenized inputs. Also, fix the dtype_for_tokenizer function returning invalid dtypes for a certain tokenizer size

--- ricode/ml/_preprocessing/tokenization.py
+++ ricode/ml/_preprocessing/tokenization.py
@@ -26,7 +26,8 @@ def dtype_for_tokenizer(tokenizer: PreTrainedTokenizerBase):
 
 @functools.lru_cache
 def _dtype_for_tokenizer(tokenizer_name_or_path: str, size: int):
-    num_bytes = math.ceil(math.log2(size)) // 8
+    num_bytes = math.ceil(math.log2(size) / 8)
+    assert num_bytes > 1, (num_bytes, size, math.log2(size) / 8)
 
     rounded_bytes = 2 ** math.ceil(math.log2(num_bytes))
     if rounded_bytes < 1 or rounded_bytes > 8:
@@ -41,7 +42,10 @@ def _sample_to_tensor(sample: np.ndarray | list, dtype: torch.dtype):
     if isinstance(sample, np.ndarray):
         tensor = torch.from_numpy(sample).to(dtype)
     else:
-        tensor = torch.tensor(sample, dtype=dtype)
+        try:
+            tensor = torch.tensor(sample, dtype=dtype)
+        except RuntimeError:
+            raise RuntimeError(f"Failed to convert {sample!r} to {dtype=!r}") from None
     return tensor
 
 
@@ -51,17 +55,37 @@ def reduce_to_num_tokens(
     return sum(map(len, batch["tokens"]))
 
 
+def tokenize_pretokenized_batch(
+    batch: MutableMapping[str, list[Any]],
+    /,
+    tokenizer: PreTrainedTokenizerBase,
+    min_length: int = 0,
+    key: str = "content",
+):
+    tokens_dtype = dtype_for_tokenizer(tokenizer)
+    samples = tokenizer(batch[key], truncation=True, is_split_into_words=True)[
+        "input_ids"
+    ]
+    tokens = [
+        _sample_to_tensor(sample, tokens_dtype)
+        for sample in samples
+        if not min_length or len(sample) >= min_length > 0
+    ]
+    return {"tokens": tokens}
+
+
 def tokenize_batch(
     batch: MutableMapping[str, list[Any]],
     /,
     tokenizer: PreTrainedTokenizerBase,
     min_length: int = 0,
+    key: str = "content",
 ):
-    tokens_dtype = _dtype_for_tokenizer(tokenizer)
-    samples = tokenizer(batch["content"], truncation=True)["input_ids"]
+    tokens_dtype = dtype_for_tokenizer(tokenizer)
+    samples = tokenizer(batch[key], truncation=True)["input_ids"]
     tokens = [
         _sample_to_tensor(sample, tokens_dtype)
         for sample in samples
-        if min_length and len(sample) >= min_length
+        if not min_length or len(sample) >= min_length > 0
     ]
     return {"tokens": tokens}
