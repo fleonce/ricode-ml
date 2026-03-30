f71714966cf5241017660b600b61571ee20c4647
fleonce <moritz.hennen@gmail.com>
2026-03-30 16:40:17 +0200

    Add PathLike as an option to iterate_json_file_type

--- ricode/utils/json_files.py
+++ ricode/utils/json_files.py
@@ -1,10 +1,13 @@
 import json
+import os
 from typing import Any, Generator
 
 import pyjson5
 
 
-def iterate_json_file_type(file_path: str) -> Generator[Any, None, None]:
+def iterate_json_file_type(
+    file_path: str | os.PathLike[str],
+) -> Generator[Any, None, None]:
     with open(file_path, "r") as f:
         if file_path.endswith(".jsonl"):
             while line := f.readline():
