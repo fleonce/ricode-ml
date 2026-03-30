e1865f3d8a479b533e8fce1e608b5bdc0fa3fb08
fleonce <moritz.hennen@gmail.com>
2026-03-30 16:48:28 +0200

    Fix AttributeError in iterate_json_file_type due to mishandled types

--- ricode/utils/json_files.py
+++ ricode/utils/json_files.py
@@ -1,20 +1,21 @@
 import json
-import os
+import pathlib
 from typing import Any, Generator
 
 import pyjson5
 
 
 def iterate_json_file_type(
-    file_path: str | os.PathLike[str],
+    file_path: str | pathlib.Path,
 ) -> Generator[Any, None, None]:
+    file_name = file_path if isinstance(file_path, str) else file_path.name
     with open(file_path, "r") as f:
-        if file_path.endswith(".jsonl"):
+        if file_name.endswith(".jsonl"):
             while line := f.readline():
                 if line.endswith("\n"):
                     line = line[: -len("\n")]
                 yield json.loads(line)
-        elif file_path.endswith(".json5"):
+        elif file_name.endswith(".json5"):
             yield from pyjson5.load(f)
         else:
             yield from json.load(f)
