d87d8951b6c41f2da2e4221f49392d9d8b2db6d0
fleonce <moritz.hennen@gmail.com>
2026-04-27 11:05:23 +0200

    Add support for storing pure python objects in `FlattenedDataset`

--- ricode/ml/datasets/cumulative.py
+++ ricode/ml/datasets/cumulative.py
@@ -1,17 +1,31 @@
 import math
 import os
 import pathlib
+import pickle
 import typing
 from collections import OrderedDict
-from typing import Mapping, MutableMapping, Optional, Sequence, TypeAlias
+from typing import (
+    Any,
+    Mapping,
+    MutableMapping,
+    MutableSequence,
+    Optional,
+    Sequence,
+    TypeAlias,
+    TypeVar,
+)
 
 import numpy as np
 import safetensors.torch
 import torch
 from more_itertools import first
-
+from more_itertools.recipes import flatten
 
 _cpu_device = torch.device("cpu")
+
+T = TypeVar("T")
+PY_BINSIZE = 10000
+_OBJECTS_FILENAME = "objects.bin"
 
 
 def dtype_for_list(values: Sequence[float] | Sequence[int]):
@@ -22,25 +36,55 @@ def dtype_for_list(values: Sequence[float] | Sequence[int]):
     return torch.int32
 
 
+def _separate_into_bins(
+    m: Mapping[str, T]
+) -> MutableMapping[str, MutableMapping[int, T]]:
+    bins = OrderedDict()
+    for key in sorted(m.keys()):
+        value = m[key]
+        name, index = key.split(".", maxsplit=1)
+        if name not in bins:
+            bins[name] = OrderedDict()
+        bins[name][int(index)] = value
+    return bins
+
+
+def _flatten_bins(m: Mapping[str, Mapping[int, T]]) -> dict[str, T]:
+    out = OrderedDict()
+    for key in sorted(m.keys()):
+        for index in sorted(m[key].keys()):
+            out[f"{key}.{index}"] = m[key][index]
+    return out
+
+
 class FlattenedDataset:
     @classmethod
     def from_preprocessed(cls, name_or_path: str | os.PathLike):
         tensors = safetensors.torch.load_file(
             os.path.join(name_or_path, "tensors.safetensors")
         )
-        return cls(tensors, None)
+        objects = None
+        objects_file = os.path.join(name_or_path, _OBJECTS_FILENAME)
+        if os.path.exists(objects_file):
+            with open(objects_file, "rb") as objects_f:
+                objects = pickle.load(objects_f)
+        return cls(tensors, objects)
 
     @classmethod
     def new_empty(cls, binsize_hints=None):
         return cls(None, None, None, binsize_hints)
 
     bins: MutableMapping[str, MutableMapping[int, torch.Tensor]]
+    py_bins: MutableMapping[str, MutableMapping[int, MutableSequence[Any]]]
     # binsize: int
     cumulative_lengths: MutableMapping[str, list[int]]
+    # track the names of the scalar field values
+    scalars: set[str]
 
     def __init__(
         self,
-        tensors: Optional[MutableMapping[str, torch.Tensor]],
+        tensors: Optional[MutableMapping[str, torch.Tensor]] = None,
+        objects: Optional[MutableMapping[str, MutableSequence[Any]]] = None,
         cumulative_lengths=None,
         binsizes=None,
         binsize_hints=None,
@@ -48,6 +92,8 @@ class FlattenedDataset:
     ):
         if tensors is None:
             tensors = {}
+        if objects is None:
+            objects = {}
         if cumulative_lengths is None:
             if len(tensors) > 0:
                 cumulative_lengths = {
@@ -64,16 +110,12 @@ class FlattenedDataset:
         self.cumulative_lengths = cumulative_lengths
         self.keys = set(cumulative_lengths.keys())
 
-        bins = {}
-        for key in sorted(tensors.keys()):
-            tensor = tensors[key]
-            name, index = key.split(".", maxsplit=1)
-            if name not in bins:
-                bins[name] = {}
-            bins[name][int(index)] = tensor
-            if name not in binsizes:
-                binsizes[name] = int(tensor.size(0))
-        self.bins = bins
+        self.bins = _separate_into_bins(tensors)
+        for name, ts in self.bins.items():
+            binsizes[name] = len(first(self.bins.values()))
+        self.py_bins = _separate_into_bins(objects)
+        self.py_keys = self.keys - set(self.bins.keys())
+
         self.max_binsize = max_binsize
         self.binsizes = binsizes
         self.binsize_hints = binsize_hints or {}
@@ -112,6 +154,7 @@ class FlattenedDataset:
                     )
                     for key, value in self.tensors.items()
                 },
+                _flatten_bins(self.py_bins),
                 self.cumulative_lengths,
                 self.binsizes,
                 self.binsize_hints,
@@ -119,10 +162,14 @@ class FlattenedDataset:
         else:
             return FlattenedDataset(
                 {key: value.to(device_or_key) for key, value in self.tensors.items()},
+                _flatten_bins(self.py_bins),
                 self.cumulative_lengths,
                 self.binsizes,
                 self.binsize_hints,
             )
+
+    def _is_py(self, key: str):
+        return key in self.py_keys
 
     def _key_len(self, key: str):
         # return the length of the specified key in terms of elements
@@ -139,8 +186,40 @@ class FlattenedDataset:
 
         result = {}
         for key in self.keys:
-            result[key] = self.__getitem__for_key(key, item)
+            if self._is_py(key):
+                item = self.__py_getitem__for_key(key, item)
+            else:
+                item = self.__getitem__for_key(key, item)
+            result[key] = item
         return result
+
+    def __py_getitem__for_key(self, key: str, item: int) -> Sequence[Any]:
+        bins = self.py_bins[key]
+        binsize = PY_BINSIZE  # hardcoded for now
+        cumulative_lengths = self.cumulative_lengths[key]
+
+        start, end = cumulative_lengths[item], cumulative_lengths[item + 1]
+        bin_start, bin_end = start // binsize, end // binsize
+        if bin_start not in bins:
+            raise IndexError(bin_start, bins.keys())
+
+        if bin_start == bin_end:
+            # quick: we found an element inside a single bin
+            value = bins[bin_start][start % binsize : end % binsize]
+        else:
+            parts = [
+                # the first part
+                bins[bin_start][start % binsize :],
+            ]
+
+            if bin_start + 1 < bin_end:
+                for bin_intermediate in range(bin_start + 1, bin_end):
+                    parts.append(bins[bin_intermediate])
+            parts.append(bins[bin_end][: end % binsize])
+            value = flatten(parts)
+
+        assert len(value) == (end - start)
+        return value
 
     def __getitem__for_key(self, key: str, item: int) -> torch.Tensor:
         bins = self.bins[key]
@@ -196,6 +275,14 @@ class FlattenedDataset:
         self.bins[key][index] = new_bin
         self.tensors[key + "." + str(index)] = new_bin
 
+    def _new_py_bin(
+        self,
+        key: str,
+    ):
+        index = len(self.py_bins[key])
+        new_bin = []
+        self.py_bins[key][index] = new_bin
+
     def append(
         self,
         tensors: Mapping[
@@ -204,7 +291,8 @@ class FlattenedDataset:
             | np.ndarray[int]
             | np.ndarray[float]
             | Sequence[int]
-            | Sequence[float],
+            | Sequence[float]
+            | Sequence[Any],
         ],
     ):
         if len(self.cumulative_lengths) == 0:
@@ -212,17 +300,32 @@ class FlattenedDataset:
             self.keys.update(set(tensors.keys()))
             for key in self.keys:
                 self.cumulative_lengths[key] = [0]
-                self.bins[key] = {}
+                if (
+                    not isinstance(tensors[key], torch.Tensor)
+                    and isinstance(tensors[key], Sequence)
+                    and not isinstance(tensors[key], np.ndarray)
+                    and not isinstance(tensors[key][0], (int, float))
+                ):
+                    # this is a pure py object, handle differently
+                    self.py_bins[key] = {}
+                    self.py_keys.add(key)
+                else:
+                    self.bins[key] = {}
 
         pre_len = len(self)
         keys = set(tensors.keys()) | self.keys
         for key in keys:
             if key not in tensors:
-                raise ValueError
+                raise ValueError(f"Missing key {key!r}")
             elif key not in self.keys:
-                raise ValueError
+                raise ValueError(
+                    f"Cannot introduce new keys after first iteration, got {key!r}, have {self.keys!r}"
+                )
 
-            self._append(key, tensors[key])
+            if self._is_py(key):
+                self._py_append(key, tensors[key])
+            else:
+                self._append(key, tensors[key])
 
         for key in tensors.keys():
             assert pre_len + 1 == self._key_len(key)
@@ -238,6 +341,37 @@ class FlattenedDataset:
         ),
     ):
         raise NotImplementedError
+
+    def _py_append(self, key: str, l: Sequence[Any]):
+        this_sequence_length = len(l)
+
+        if self._key_len(key) == 0:
+            # we insert the first element for this key
+            self._new_py_bin(key)
+
+        binsize = PY_BINSIZE
+        cumulative_lengths = self.cumulative_lengths[key]
+        bins = self.py_bins[key]
+        num_bins = len(bins)
+
+        active_position = cumulative_lengths[-1] % binsize
+        new_position = active_position + this_sequence_length
+        must_split = new_position >= binsize
+        last_bin = bins[num_bins - 1]
+        if not must_split:
+            last_bin[active_position : active_position + this_sequence_length] = l
+        else:
+            items_in_last_bin = binsize - (new_position % binsize)
+            last_bin[items_in_last_bin:] = l[:items_in_last_bin]
+            remainder = l[items_in_last_bin:]
+            while len(remainder) > 0:
+                self._new_py_bin(key)
+                last_bin = bins[len(bins) - 1]
+                if len(remainder) >= binsize:
+                    last_bin[:] = l[:binsize]
+                else:
+                    last_bin[: len(remainder)] = l
+        cumulative_lengths.append(cumulative_lengths[-1] + this_sequence_length)
 
     def _append(
         self, key: str, tensor_or_list: torch.Tensor | Sequence[int] | Sequence[float]
@@ -302,13 +436,7 @@ class FlattenedDataset:
         cumulative_lengths.append(cumulative_lengths[-1] + this_sequence_length)
 
     def save_to_disk(self, foldername: str | pathlib.Path):
-        tensors = OrderedDict(
-            {
-                key + "." + str(index): tensor
-                for key, bins in self.bins.items()
-                for index, tensor in bins.items()
-            }
-        )
+        tensors = _flatten_bins(self.bins)
 
         for key, cumulative_lengths in self.cumulative_lengths.items():
             tensors[key + "_cumulative_length"] = torch.tensor(cumulative_lengths)
@@ -319,6 +447,11 @@ class FlattenedDataset:
         safetensors.torch.save_file(
             tensors, os.path.join(foldername, "tensors.safetensors"), None
         )
+
+        if len(self.py_bins) > 0:
+            objects = _flatten_bins(self.py_bins)
+            with open(os.path.join(foldername, _OBJECTS_FILENAME), "wb") as objects_f:
+                pickle.dump(objects, objects_f)
 
 
 class FlattenedDatasetDict(dict[str, FlattenedDataset]):
