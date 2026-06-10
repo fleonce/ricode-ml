2bf9e42ee0f58ff93a437b75143c096962f455ec
fleonce <mail@fleance.de>
2026-06-10 20:10:53 +0200

    Remove temporary files correctly

--- ricode/utils/tempfiles.py
+++ ricode/utils/tempfiles.py
@@ -1,6 +1,7 @@
 import atexit
+import os
 import shutil
-from tempfile import mkdtemp, mktemp, mkstemp
+from tempfile import mkdtemp, mkstemp
 from typing import Literal
 
 import attrs
@@ -32,9 +33,9 @@ class TemporaryFile:
     def __enter__(self) -> tuple[int, str]:
         self.fd, self.name = mkstemp(self.suffix)
         if self.delete_at == "exit":
-            atexit.register(shutil.rmtree, self.name)
+            atexit.register(os.remove, self.name)
         return self.fd, self.name
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.delete_at == "content-exit":
-            shutil.rmtree(self.name)
+            os.remove(self.name)
